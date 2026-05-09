af9968c06f4367c0c05e5e2ed9932868a869c6c3
brandonmacknl <contact@brandonmack.nl>
2026-05-09 21:14:51 +0200

    modified:   backend/src/config/db.js ssl configuration added to database connection pool

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,6 +8,9 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 module.exports = pool;
