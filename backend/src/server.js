8f713b2ec5701f904b5fd3b6d3d53bb525c697ec
jesse-infra <577077@student.fontys.nl>
2026-06-02 18:46:06 +0200

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
+const express = require("express");
 const cors = require("cors");
-const express = require("express"); 
-const corsOptions = {
+
+const app = express();
+
+app.use(cors({
   origin: "https://yoroomie.azurewebsites.net",
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
-};
+}));
+
+app.use(express.json());
 
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
